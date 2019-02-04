d8a5b515de588023340ca25446f7fd40433eafed
Luana <luanachen@gmail.com>
2019-02-04 10:14:51 -0200

    Initial commit
    Signed-off-by: Luana <luanachen@gmail.com>

---
+++ awesomeBeers/awesomeBeers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  awesomeBeers
+//
+//  Created by Luana Chen on 04/02/19.
+//  Copyright © 2019 Luana Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
