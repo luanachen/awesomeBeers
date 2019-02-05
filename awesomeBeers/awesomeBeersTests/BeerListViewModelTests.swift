4444e4762f962a3195b84e314f44c3a83578a117
Luana <luanachen@gmail.com>
2019-02-05 13:01:30 -0200

    feat(UnitTest): add viewModels unit test

---
+++ awesomeBeers/awesomeBeersTests/BeerListViewModelTests.swift
@@ -0,0 +1,44 @@
+//
+//  BeerListViewModelTests.swift
+//  protocolBeerTests
+//
+//  Created by Luana Chen on 05/02/19.
+//  Copyright © 2019 Luana Chen. All rights reserved.
+//
+
+import XCTest
+
+class BeerListViewModelTests: XCTestCase {
+    
+    var sut: BeerListViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        sut = BeerListViewModel()
+        sut.loadBeers()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testGetNumberOfItemsNotNil() {
+        XCTAssertNotNil(sut.getNumberOfItems())
+    }
+    
+    func testBeerNameAtFirstIndexEqualToBuzz() {
+        let beer = sut.getBeer(for: 0)
+        
+        XCTAssertEqual(beer?.name, "Buzz")
+    }
+    
+    func testGetBeerInfCorrectly() {
+        guard let beer = sut.getBeer(for: 0) else { return }
+        XCTAssertEqual(beer.id, 1)
+        XCTAssertEqual(beer.name, "Buzz")
+        XCTAssertEqual(beer.description, "A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once.")
+        XCTAssertEqual(beer.imageURL, "https://images.punkapi.com/v2/keg.png")
+    }
+
+}
