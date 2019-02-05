d0d8e12cfd38ec57b0eff8e37bfca39f51b659a0
Luana <luanachen@gmail.com>
2019-02-05 13:01:56 -0200

    feat(UiTests): add UI tests

--- awesomeBeers/awesomeBeersUITests/awesomeBeersUITests.swift
+++ awesomeBeers/awesomeBeersUITests/awesomeBeersUITests.swift
@@ -9,26 +9,62 @@
 import XCTest
 
 class awesomeBeersUITests: XCTestCase {
-
+    
+    var app: XCUIApplication!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        super.setUp()
+        
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        
+        app = XCUIApplication()
+        
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        super.tearDown()
     }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSelectFirstItemAndBackToList() {
+        app.launch()
+        
+        XCTAssertTrue(app.isDisplayingBeerListView)
+        
+        XCTAssertTrue(app.isDisplayingBeerWithNameBuzz)
+        
+        app.collectionViews.cells.otherElements.containing(.staticText, identifier:"Buzz").element.tap()
+        
+        XCTAssertTrue(app.isDisplayingBeerDetailView)
+        
+        XCTAssertTrue(app.isDisplayingNavigationBarWithNameBuzz)
+        
+        app.navigationBars["Buzz"].buttons["Awesome Beers"].tap()
+        
+        XCTAssertTrue(app.isDisplayingNavigationBarWithNameAwesomeBeers)
     }
+    
+}
 
+extension XCUIApplication {
+    var isDisplayingBeerListView: Bool {
+        return otherElements["beerListView"].exists
+    }
+    
+    var isDisplayingBeerDetailView: Bool {
+        return otherElements["beerDetailView"].exists
+    }
+    
+    var isDisplayingBeerWithNameBuzz: Bool {
+        return collectionViews.cells.otherElements.containing(.staticText, identifier:"Buzz").element.exists
+    }
+    
+    var isDisplayingNavigationBarWithNameBuzz: Bool {
+        return otherElements.containing(.navigationBar, identifier:"Buzz").element.exists
+    }
+    
+    var isDisplayingNavigationBarWithNameAwesomeBeers: Bool {
+        return  otherElements.containing(.navigationBar, identifier:"Awesome Beers").element.exists
+    }
 }
